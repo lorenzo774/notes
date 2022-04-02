4f6af8d71af77da0c14c581e3ecbfd88e0b350dd
lorenzo774 <lorenzoconti774@gmail.com>
2022-04-02 14:53:18 +0200

    Modified the service worker to cache site assets

---
+++ public/sw.js
@@ -0,0 +1,33 @@
+const cacheName = `notes`;
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache
+        .addAll([
+          `/`,
+          `/index.html`,
+          `/global.css`,
+          `/favicon.ico`,
+          `/build/bundle.css`,
+          `/build/bundle.js`,
+          `/build/bundle.js.map`,
+        ])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then((cache) => cache.match(event.request, { ignoreSearch: true }))
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
